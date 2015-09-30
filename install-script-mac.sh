89c340aa26e4e687ed0227b10237e01137339136
Michael Steger <mjsteger1@gmail.com>
2015-09-30 11:05:07 -0700

    Update simple install script

--- install-script-mac.sh
+++ install-script-mac.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-if [ -z $(which brew) ]; then
+# TODO ugly hack, but brew doesn't like "which"
+if [ ! -f "/usr/local/bin/brew" ]; then
     # Install brew
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 git submodule init && git submodule update
+
+./install-dot-files -g
